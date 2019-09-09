6de4aad687affc22669d59cb1f2049e6315c6025
Fu, Biye <biye.fu@outlook.com>
2019-09-09 21:06:13 +0800

    init commit

---
+++ src/main/java/com/biye/rws/WebfluxWebsocketSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.biye.rws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxWebsocketSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxWebsocketSpringBootStarterApplication.class, args);
+	}
+
+}
