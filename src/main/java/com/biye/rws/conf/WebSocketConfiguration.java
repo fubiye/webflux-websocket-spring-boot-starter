4cf7acd1b0f931d211ba9f2c997127a940ee7fab
Fu, Biye <biye.fu@outlook.com>
2019-09-09 22:27:43 +0800

    add auto configured mapping handler

--- src/main/java/com/biye/rws/conf/WebSocketConfiguration.java
+++ src/main/java/com/biye/rws/conf/WebSocketConfiguration.java
@@ -1,10 +1,15 @@
 package com.biye.rws.conf;
 
+import com.biye.rws.annotation.WsController;
 import com.biye.rws.controller.EchoWsController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.HandlerMapping;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
@@ -15,9 +20,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Component
-public class WebSocketConfiguration {
+public class WebSocketConfiguration implements ApplicationContextAware {
 
     private Logger logger = LoggerFactory.getLogger(WebSocketConfiguration.class);
+
+    private ApplicationContext ctx;
 
     @Bean
     public WebSocketHandler echoHandler(){
@@ -29,6 +36,14 @@ public class WebSocketConfiguration {
         Map<String,WebSocketHandler> pathToHanlder = new HashMap<>();
         pathToHanlder.put("/ws/echo",echoHandler());
         logger.info("Add handler mapping: /ws/echo ...");
+        printStartupInfos();
+        String[] beanNames = ctx.getBeanNamesForAnnotation(WsController.class);
+        for(String beanName: beanNames){
+            logger.info("bean name: " + beanName);
+            WebSocketHandler handler = ctx.getBean(beanName, WebSocketHandler.class);
+            WsController controller = AnnotatedElementUtils.findMergedAnnotation(handler.getClass(), WsController.class);
+            pathToHanlder.put(controller.value(),handler);
+        }
         SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
         mapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
         mapping.setUrlMap(pathToHanlder);
@@ -40,4 +55,14 @@ public class WebSocketConfiguration {
         return new WebSocketHandlerAdapter();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ctx = applicationContext;
+    }
+
+    private void printStartupInfos(){
+        logger.info("******************************************************************");
+        logger.info("*  Auto configuring WebSocket Handlers......");
+        logger.info("******************************************************************");
+    }
 }
